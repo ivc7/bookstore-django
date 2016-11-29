6a069853454be0426df86e4d00e2684b0718151b
ivziete <ivziete@gmail.com>
2016-11-29 10:48:44 -0500

    Add Author and review models, link book to author, update admin to match, create book detail page with url, view and template

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author','price', 'stock', )
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name','first_name')
--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.store, name='index'),
+    url(r'^book/(\d+)',views.book_detail,name='book_details')
 ]
