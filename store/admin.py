580796e2d0bc9851fd7b5f6862f9c836dbc4678c
ivziete <ivziete@gmail.com>
2016-11-29 12:14:52 -0500

    Add models, views, templates and URLs for shopping cart

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Author
+from .models import Book, Author, BookOrder, Cart
 
 
 @admin.register(Book)
@@ -10,3 +10,13 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name','first_name')
+
+
+@admin.register(BookOrder)
+class BookOrderAdmin(admin.ModelAdmin):
+    list_display = ('book', 'cart', 'quantity')
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'active', 'order_date')
